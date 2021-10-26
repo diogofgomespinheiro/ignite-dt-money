be48c235c7f6fe8aa5fb33fece46d4ab6426bf0c
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-10-27 00:09:30 +0100

    ✨ feat: add miragejs

--- src/components/transactions-table/transactions-table.tsx
+++ src/components/transactions-table/transactions-table.tsx
@@ -1,14 +1,24 @@
+import { useEffect } from 'react';
+
 import { Container } from './styles';
 
 function TransactionsTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <Container>
       <table>
         <thead>
-          <th>Title</th>
-          <th>Value</th>
-          <th>Category</th>
-          <th>Date</th>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
         </thead>
         <tbody>
           <tr>
