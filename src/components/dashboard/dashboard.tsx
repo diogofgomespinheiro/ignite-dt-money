da443c7109df5ba014bc67e233ed366cf1c619e2
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-10-26 23:35:00 +0100

    ✨ feat: create dashboard component

---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+import Summary from '../summary';
+
+import { Container } from './styles';
+
+function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
+
+export default Dashboard;
